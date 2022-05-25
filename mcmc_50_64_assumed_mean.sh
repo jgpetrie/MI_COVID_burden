37692f5bc297bfb7ca3c23c4c7c60f0c9bb878f6
jgpetrie <petrie.joshua@marshfieldresearch.org>
2022-05-25 16:56:00 -0500

    Add files via upload

---
+++ mcmc_50_64_assumed_mean.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#SBATCH --job-name=mcmc_50_64_assumed_mean
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --mem-per-cpu=32gb
+#SBATCH --time=48:00:00
+#SBATCH --partition=largemem
+#SBATCH --get-user-env
+
+echo Running job name $SLURM_JOB_NAME with ID $SLURM_JOB_ID on host $SLURM_SUBMIT_HOST;
+echo Working directory is $SLURM_SUBMIT_DIR
+echo Start time: `date`;
+
+
+cd $SLURM_SUBMIT_DIR
+
+age=50-64
+mean=7.550186
+sd=105.3182
+burn=10000
+runs=50000
+s1=1
+s2=.5
+s3=.25
+s4=.7
+s5=4
+
+Rscript "MCMC assumed mean.R" $age $mean $sd $burn $runs $s1 $s2 $s3 $s4 $s5
+Rscript "MCMC summary assumed mean.R" $age $mean $sd $burn $runs
+
+echo Finish time: `date`
